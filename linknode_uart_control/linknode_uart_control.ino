cfead924671608457b62359885970ca864b8db66
steve <sjkklein@gmail.com>
2019-03-10 18:22:56 +0000

    add arduino code for outlet box

---
+++ linknode_uart_control/linknode_uart_control.ino
@@ -0,0 +1,19 @@
+int relay0 = 14;
+int relay1 = 13;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relay0, OUTPUT);
+  pinMode(relay1, OUTPUT);
+}
+
+int switch_num = relay0;
+void loop() {
+  switch(Serial.read()) {
+    case '1': switch_num = relay0; break;
+    case '2': switch_num = relay1; break;
+    case '+': digitalWrite(switch_num, HIGH); break;
+    case '-': digitalWrite(switch_num, LOW); break;
+    default: break; /* do nothing */
+  }
+}
